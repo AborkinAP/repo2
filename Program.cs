6a78c56c40acfa6e2e6757dc12d14bfd3a577d7e
Andrey <andrey270304@gmail.com>
2022-08-10 16:16:59 +0300

    added program

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿//Задача 10: программа показывает вторую цифру трехзначного числа
+// int num = new Random().Next(100, 1000);
+// Console.WriteLine($"{num}");
+// int second = (num % 100) / 10;
+// Console.WriteLine($"{second} - второе число");
+
+// Задача 13: Определение 3 цифры числа
+// int num = new Random().Next(1, 1000000);
+// Console.WriteLine($"{num}");
+// int count = 10;
+// int n; // третья цифра
+// if (num / count < count)
+// {
+//     Console.WriteLine($"{num} третьей цифры нет");
+// }
+// else
+// {
+//     while ((num / count) > 100)
+//     {
+//         count = count * 10;
+//     }
+//     n = num % count;
+//     if (n > 9)
+//     {
+//         count = count / 10;
+//         n = n / count;
+//         Console.WriteLine($"{n} -  третья цифра");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{n} - третья цифра");
+//     }
+// }
+
+// Задача 15: проверка цифры с выходным днем
+// int num = new Random().Next(1, 8);
+// Console.WriteLine($"{num}");
+// if(num == 7 || num == 6)
+// {
+// Console.WriteLine($"{num} - да");
+// }
+// else
+// {
+// Console.WriteLine($"{num} - нет");
+// 
